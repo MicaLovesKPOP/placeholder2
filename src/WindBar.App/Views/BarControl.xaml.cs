4792256e0bd77083446be092100baf8ca3fee750
Mica <25252229+MicaLovesKPOP@users.noreply.github.com>
2026-06-24 23:03:58 +0200

    Add BarControl.xaml.cs

---
+++ src/WindBar.App/Views/BarControl.xaml.cs
@@ -0,0 +1,10 @@
+namespace WindBar.App.Views
+{
+    public partial class BarControl : System.Windows.Controls.UserControl
+    {
+        public BarControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
