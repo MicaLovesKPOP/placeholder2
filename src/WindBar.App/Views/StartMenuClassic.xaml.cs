b4550b42f4eb35052e593d300d9e8d1d15ecade9
Mica <25252229+MicaLovesKPOP@users.noreply.github.com>
2026-06-24 23:29:43 +0200

    Connect classic start provider to app scanner

--- src/WindBar.App/Views/StartMenuClassic.xaml.cs
+++ src/WindBar.App/Views/StartMenuClassic.xaml.cs
@@ -1,24 +1,22 @@
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using WindBar.Core;
 
 namespace WindBar.App.Views
 {
     public partial class StartMenuClassic : UserControl, IStartMenuProvider
     {
+        private readonly AppScanner _scanner = new AppScanner();
+
         public StartMenuClassic()
         {
-            Content = new TextBlock
-            {
-                Text = "Classic Start, re-imagined for Windows 11",
-                Foreground = Brushes.White,
-                FontSize = 24,
-                Margin = new Thickness(24)
-            };
+            Content = CreateView();
         }
 
         public string DisplayName => "Classic Start";
-        public object CreateView() => this;
+
+        public object CreateView()
+        {
+            return new StartSurface("Classic Start", _scanner.ScanSmartDefaults());
+        }
     }
 }
