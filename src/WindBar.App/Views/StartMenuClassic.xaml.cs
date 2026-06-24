252530e4244e60438ce3fa370a55596fc7c08fac
Mica <25252229+MicaLovesKPOP@users.noreply.github.com>
2026-06-24 23:23:31 +0200

    Make classic start provider code-only

--- src/WindBar.App/Views/StartMenuClassic.xaml.cs
+++ src/WindBar.App/Views/StartMenuClassic.xaml.cs
@@ -1,19 +1,24 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using WindBar.Core;
 
 namespace WindBar.App.Views
 {
-    public partial class StartMenuClassic : System.Windows.Controls.UserControl, IStartMenuProvider
+    public partial class StartMenuClassic : UserControl, IStartMenuProvider
     {
         public StartMenuClassic()
         {
-            InitializeComponent();
+            Content = new TextBlock
+            {
+                Text = "Classic Start, re-imagined for Windows 11",
+                Foreground = Brushes.White,
+                FontSize = 24,
+                Margin = new Thickness(24)
+            };
         }
 
         public string DisplayName => "Classic Start";
-
-        public object CreateView()
-        {
-            return this;
-        }
+        public object CreateView() => this;
     }
 }
